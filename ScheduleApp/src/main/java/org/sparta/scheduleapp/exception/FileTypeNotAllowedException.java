9c040d0ed8b47e79a4710e630a1a94f33d3edc2e
dami0806 <dami0804@naver.com>
2024-05-15 21:15:03 +0900

    feat: file upload download

---
+++ ScheduleApp/src/main/java/org/sparta/scheduleapp/exception/FileTypeNotAllowedException.java
@@ -0,0 +1,8 @@
+package org.sparta.scheduleapp.exception;
+
+public class FileTypeNotAllowedException extends RuntimeException {
+
+    public FileTypeNotAllowedException(String message) {
+        super(message);
+    }
+}
