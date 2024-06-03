b2ef1d115e6718f4d2d4c4e2dba44a35e7d1ae97
dami0806 <dami0804@naver.com>
2024-06-03 17:30:19 +0900

    refactor: 피드백 수정
    단일 책임 원칙에 맞게 수정

--- src/main/java/com/sparta/scheduleapp/schedule/service/ScheduleService.java
+++ src/main/java/com/sparta/scheduleapp/schedule/service/ScheduleService.java
@@ -3,6 +3,7 @@ package com.sparta.scheduleapp.schedule.service;
 import com.sparta.scheduleapp.exception.*;
 import com.sparta.scheduleapp.exception.message.file.FileNotSaveException;
 import com.sparta.scheduleapp.exception.message.file.FileTypeNotAllowedException;
+import com.sparta.scheduleapp.file.service.FileService;
 import com.sparta.scheduleapp.schedule.dto.ScheduleRequestDto;
 import com.sparta.scheduleapp.exception.message.ErrorMessage;
 import com.sparta.scheduleapp.file.entity.File;
@@ -12,24 +13,27 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.sparta.scheduleapp.schedule.entity.Schedule;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
-    private final FileRepository fileRepository;
 
+    @Qualifier("fileServiceImpl")
+    private final FileService fileService;
 
     // 스케줄 생성
-    public Schedule createSchedule(ScheduleRequestDto requestDto,MultipartFile file, String username){
-        File fileEntity = saveFile(file);
+    public Schedule createSchedule(ScheduleRequestDto requestDto, MultipartFile file, String username) {
+        File fileEntity = fileService.saveFile(file);
 
         Schedule schedule = Schedule.builder()
                 .title(requestDto.getTitle())
@@ -43,7 +47,6 @@ public class ScheduleService {
 
         return scheduleRepository.save(schedule);
     }
-
 
 
     public List<Schedule> getScheduleList() {
@@ -69,7 +72,7 @@ public class ScheduleService {
     @Transactional
     public Schedule updateSchedule(Long id, ScheduleRequestDto requestDto, MultipartFile file, String username) {
         Schedule schedule = findSchedule(id);
-        File savedFile = saveFile(file);
+        File savedFile = fileService.saveFile(file);
 
         if (!schedule.getCreator().equals(username)) {
             log.error("권한이 없습니다: {}", username);
@@ -105,42 +108,5 @@ public class ScheduleService {
             log.error("Schedule not found with id: {}", id);
             return new ScheduleNotFoundException(ErrorMessage.SCHEDULE_NOT_FOUND);
         });
-    }
-
-    //저장
-    private File saveFile(MultipartFile file) {
-        // 파일이 null인지, 있는데 빈건지
-        if(file != null && !file.isEmpty()) {
-            try {
-                validateFile(file);
-                return File.builder()
-                        .fileName(file.getOriginalFilename())
-                        .fileExtension(getFileExtension(file.getOriginalFilename()))
-                        .fileSize(file.getSize())
-                        .createdDate(LocalDateTime.now())
-                        .fileContent(file.getBytes())
-                        .build();
-            }catch (IOException e) {
-                log.error("파일 저장 중 오류 발생: {}", e.getMessage());
-                throw new FileNotSaveException("파일 저장 중 오류 발생");
-            }
-        }
-        return null;
-    }
-
-    // 파일 형식 용량 체크
-    private void validateFile(MultipartFile file) {
-        String fileType = getFileExtension(file.getOriginalFilename());
-        if (!fileType.equalsIgnoreCase("png") && !fileType.equalsIgnoreCase("jpg") && !fileType.equalsIgnoreCase("jpeg")) {
-            throw new FileTypeNotAllowedException("파일 형식은 png, jpg, jpeg로 구성됩니다.");
-        }
-        if(file.getSize() > 5*1024*1024) {
-            throw new FileNotSaveException("파일 크기은 최대 5MB 까지만 가능합니다.");
-        }
-    }
-
-    // 파일 확장자 가져오기
-    private String getFileExtension(String fileName) {
-        return fileName.substring(fileName.lastIndexOf('.') + 1);
     }
 }
